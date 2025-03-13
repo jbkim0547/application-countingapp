7eb6c04b089a83cf057cc713c610e398d0b2283b
Jay Kim <jbkim3510@gmail.com>
2025-03-13 14:51:05 -0700

    calendar css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import * as React from 'react'
 import Count from './ApplicationCount/Count';
 import NavBar from './Navbars/NavBar';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Calendar from './Calendar/Calendar';
+import './Calendar/Calendar.css'
+
 
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
@@ -16,7 +19,15 @@ function App() {
           <Route path="/count" element={<Count />} />
         </Routes>
       </Router>
+    <div>
+    <br></br>
+    </div>
+      <div className="calendarContainer">
+      <Calendar />
+      </div>
     </React.Fragment>
+
+
 
   );
 }
