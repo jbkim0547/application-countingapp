3c8dfccd906055fbca2ff49db3a6db5dfa3b5a83
Jay Kim <jbkim3510@gmail.com>
2025-03-13 16:12:00 -0700

    AddingEventPopUp

--- src/Calendar/Calendar.jsx
+++ src/Calendar/Calendar.jsx
@@ -1,87 +1,121 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Calendar = () => {
 
-    
+    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    const monthOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    const currentDate = new Date()
+
+    const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
+    const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
+    const [selectedDate, setSelectedDate] = useState(currentDate)
+    const [showEventPopup, setShowEventPopup] = useState(false)
+    const [events, setEvents] = useState([])
+    const [eventTime, setEventTime] = useState({ hours: '00', minutes: "00" })
+    const [eventText, setEventText] = useState('')
+
+    const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate()
+    const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDate()
+
+    const prevMonth = () => {
+        setCurrentMonth((prevMonth) => (prevMonth === 0 ? 11 : prevMonth - 1))
+        setCurrentYear((prevYear) => (currentMonth === 0 ? prevYear - 1 : prevYear))
+    }
+
+    const nextMonth = () => {
+        setCurrentMonth((prevMonth) => (prevMonth === 11 ? 0 : prevMonth + 1))
+        setCurrentYear((prevYear) => (currentMonth === 11 ? prevYear + 1 : prevYear))
+    }
+
+    const handleDayClick = (day) => {
+        const clickedDate = new Date(currentYear, currentMonth, day)
+        const today = new Date()
+
+        setSelectedDate(clickedDate)
+        setShowEventPopup(true)
+        setEventText("")
+        setEventTime({ hours: '00', minutes: "00" })
+
+        // if (clickedDate >= today) {
+        //     setSelectedDate(clickedDate)
+        //     setShowEventPopup(true)
+        // }
+    }
+
+    const isSameDay = (date1, date2) => {
+        return (
+            date1.getFullYear() === date2.getFullYear() &&
+            date1.getMonth() === date2.getMonth() &&
+            date1.getDate() === date2.getDate()
+        )
+    }
+
+    const handleEventSubmit = () => {
+        const newEvent = {
+            date: selectedDate,
+            time: `${eventTime.hours.padStart(2, '0')}:${eventTime.minutes.padStart(2, '0')}`,
+            text: eventText,
+        }
+
+        setEvents([...events, newEvent])
+        setEventTime({ hours: '00', minutes: "00" })
+        setEventText("")
+        setShowEventPopup(false)
+    }
+
     return (
         <div className='appliation-calendar'>
             <div className='calendar'>
                 <h1 className='heading' >Caldenar</h1>
                 <div className='navigate-date'>
-                    <h2 className='month'> May, </h2>
-                    <h2 className='year'>2024</h2>
+                    <h2 className='month'> {monthOfYear[currentMonth]},</h2>
+                    <h2 className='year'>{currentYear}</h2>
                     <div className='buttons'>
-                        <i className="bx bx-chevron-left"></i>
-                        <i className="bx bx-chevron-right"></i>
+                        <i className="bx bx-chevron-left" onClick={prevMonth}></i>
+                        <i className="bx bx-chevron-right" onClick={nextMonth}></i>
                     </div>
                 </div>
                 <div className="weekdays">
-                    <span>Sun</span>
-                    <span>Mon</span>
-                    <span>Tue</span>
-                    <span>Wed</span>
-                    <span>Thu</span>
-                    <span>Fri</span>
-                    <span>Sat</span>
+                    {daysOfWeek.map((day) => <span key={day}>{day}</span>)}
                 </div>
                 <div className="days">
-                    <span>1</span>
-                    <span>2</span>
-                    <span>3</span>
-                    <span>4</span>
-                    <span>5</span>
-                    <span>6</span>
-                    <span>7</span>
-                    <span>8</span>
-                    <span>9</span>
-                    <span>10</span>
-                    <span>11</span>
-                    <span>12</span>
-                    <span>13</span>
-                    <span>14</span>
-                    <span>15</span>
-                    <span>16</span>
-                    <span className='current-day'>17</span>
-                    <span>18</span>
-                    <span>19</span>
-                    <span>20</span>
-                    <span>21</span>
-                    <span>22</span>
-                    <span>23</span>
-                    <span>24</span>
-                    <span>25</span>
-                    <span>26</span>
-                    <span>27</span>
-                    <span>28</span>
-                    <span>29</span>
-                    <span>30</span>
-                    <span>31</span>
+                    {[...Array(firstDayOfMonth).keys()].map((_, index) => <span key={`empty-${index}`} />)}
+                    {[...Array(daysInMonth).keys()].map((day) => <span key={day + 1} className={day + 1 === currentDate.getDate() && currentMonth === currentDate.getMonth() &&
+                        currentYear === currentDate.getFullYear() ? 'current-day' : ''} onClick={() => handleDayClick(day + 1)}>{day + 1}</span>)}
                 </div>
             </div>
             <div className="events">
-                <div className="event-popup">
+                {showEventPopup && (<div className="event-popup">
                     <div className="time-input">
                         <div className="event-popup-time">Time</div>
-                        <input type="number" name= "hours" min={0} max={24} className='hours' />
-                        <input type="number" name= "minutes" min={0} max ={60} className='hours' />
+                        <input type="number" name="hours" min={0} max={24} className='hours' value={eventTime.hours} onChange={(e) => setEventTime({ ...eventTime, hours: e.target.value })} />
+                        <input type="number" name="minutes" min={0} max={60} className='hours' value={eventTime.minutes} onChange={(e) => setEventTime({ ...eventTime, minutes: e.target.value })} />
                     </div>
-                    <textarea placeholder='Enter Event Text (Maximum 60)'></textarea>
-                    <button className="event-popup-btn">Add Event</button>
-                    <button className='close-event-popup'>
+                    <textarea placeholder='Enter Event Text (Maximum 60)' value={eventText} onChange={(e) => {
+                        if (e.target.value.length <= 60) {
+                            setEventText(e.target.value)
+                        }
+                    }}></textarea>
+                    <button className="event-popup-btn" onClick={handleEventSubmit}>Add Event</button>
+                    <button className='close-event-popup' onClick={() => setShowEventPopup(false)}>
                         <i className="bx bx-x"></i>
                     </button>
                 </div>
-                <div className="event">
-                    <div className="event-date-wrapper">
-                        <div className="event-date">March 13, 2025</div>
-                        <div className="event-time">10:00</div>
+                )}
+                {events.map((event, index) => (
+                    <div className="event" key={index}>
+                        <div className="event-date-wrapper">
+                            <div className="event-date">{`${monthOfYear[event.date.getMonth()]} ${event.date.getDate()}, ${event.date.getFullYear()}`}</div>
+                            <div className="event-time">{event.time}</div>
+                        </div>
+                        <div className="event-text">{event.text}</div>
+                        <div className="event-buttons">
+                            <i className="bx bxs-edit-alt"></i>
+                            <i className="bx bxs-message-alt-x"></i>
+                        </div>
                     </div>
-                    <div className="event-text">Meeting with John</div>
-                    <div className="event-buttons">
-                        <i className="bx bxs-edit-alt"></i>
-                        <i className="bx bxs-message-alt-x"></i>
-                    </div>
-                </div>
+                ))}
+
             </div>
         </div>
     )
