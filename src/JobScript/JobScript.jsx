4746b83ef7cfcee04d86358ba843ea51065d5068
Jay Kim <jbkim3510@gmail.com>
2025-03-25 12:36:48 -0700

    committing for jobscript

--- src/JobScript/JobScript.jsx
+++ src/JobScript/JobScript.jsx
@@ -1,46 +1,52 @@
+import "./JobScript.css";
+
 const JobScript = () => {
   return (
-    <textarea
-      defaultValue=" LinkedIn profile
-https://www.linkedin.com/in/jb-kim-14a820132/
-
-Github Profile
-https://github.com/jbkim0547/JB-kim
-
-Hello Kelly, hope I can connect you and join your successful career path!
-
-It's nice to connect Trvor. I was wondering if you can give me a HUGE favor and give me an advice or help for the position that I applied for OpenDoor.
-
-I recently applied for Aruba as a software engineer position, and I thought it would be a great idea to talk to someone who is actually working there to get ANY advices regarding interview and further process. ANY advice or support would be appreciated!
-
-Also, I am not sure if it is appropriate to ask since we just connected in the Linkedin today but do you think you could refer me or pass my info to the hiring manager for the position that I applied? That would mean a lot to me as well!
-
-Below is the position that I applied just in case you want to take a look.
-
-
-I am looking for new opportunities that have right people to work with and contribute my skillset and grow along with the team and company.
-
-As a passionate and talented employee, I would like to contribute my skillset to the team and company so I can grow along with the Team!
-
-
-Hello Sev1Tech!
-
-My name is Jay Kim and I am interested in the software engineer position that I applied for Sev1Tech. (please see the link below) And I was wondering if you could help me to pass my resume to the hiring manager. It would be greatly appreciated! 
-
-https://www.linkedin.com/jobs/view/4090121833/?alternateChannel=search&refId=Jh2l1LAM9mOIYOE6rC1YuA%3D%3D&trackingId=jFlGjk%2FkblvdN4XxkfAQng%3D%3D
-
-Thank you!
-Jay Kim
-
-
-Source
-interviewcake
-CanVa.com
-Leecode
-Pramp - interveiw prep
-
-"
-    />
+    <>
+      <div className="title">Job Description</div>
+      <div className="jobScript">
+        LinkedIn profile https://www.linkedin.com/in/jb-kim-14a820132/ <br />{" "}
+        <br />
+        Github Profile https://github.com/jbkim0547/JB-kim <br /> <br />
+        Hello Kelly, hope I can connect you and join your successful career
+        path! <br /> <br />
+        It's nice to connect Trvor. I was wondering if you can give me a HUGE
+        favor and give me an advice or help for the position that I applied for
+        OpenDoor. <br /> <br />
+        I recently applied for Aruba as a software engineer position, and I
+        thought it would be a great idea to talk to someone who is actually
+        working there to get ANY advices regarding interview and further
+        process. ANY advice or support would be appreciated!
+        <br /> <br />
+        Also, I am not sure if it is appropriate to ask since we just connected
+        in the Linkedin today but do you think you could refer me or pass my
+        info to the hiring manager for the position that I applied? That would
+        mean a lot to me as well! <br /> <br />
+        I’d like to share that I’m someone who truly enjoys building solutions
+        that make an impact. Whether it’s improving internal efficiency or
+        creating user-friendly experiences, I take pride in writing clean,
+        maintainable code and continuously learning new technologies. Beyond my
+        technical background, I bring strong collaboration skills and a
+        proactive mindset. I’ve worked cross-functionally with product owners,
+        QA teams, and designers, and I believe that great software comes from
+        great teamwork. <br /> <br />
+        Hello Sev1Tech!
+        <br /> <br />
+        My name is Jay Kim and I am interested in the software engineer position
+        that I applied for Sev1Tech. (please see the link below) And I was
+        wondering if you could help me to pass my resume to the hiring manager.
+        It would be greatly appreciated! <br /> <br />
+        https://www.linkedin.com/jobs/view/4090121833/?alternateChannel=search&refId=Jh2l1LAM9mOIYOE6rC1YuA%3D%3D&trackingId=jFlGjk%2FkblvdN4XxkfAQng%3D%3D{" "}
+        <br /> <br />
+        Thank you!
+        <br />
+        Jay Kim Source <br /> <br />
+        interviewcake <br />
+        CanVa.com <br />
+        Leecode <br />
+        Pramp - interveiw prep <br />
+      </div>
+    </>
   );
 };
 
