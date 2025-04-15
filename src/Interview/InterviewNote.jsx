2ea33edea00d6a6107590402b06adb066edaaa5c
Jay Kim <jbkim3510@gmail.com>
2025-04-15 14:07:56 -0700

    list of company name

--- src/Interview/InterviewNote.jsx
+++ src/Interview/InterviewNote.jsx
@@ -1,15 +1,53 @@
-import "./InterviewNote.css"
+import "./InterviewNote.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import IconButton from "@mui/material/IconButton";
+import ListItemText from "@mui/material/ListItemText";
+import CommentIcon from "@mui/icons-material/Comment";
 
 const InterviewNote = () => {
+  const [companyName, setCompanyName] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5065/api/duringInterviewNote")
+      .then((response) => {
+        setCompanyName(response.data);
+        companyName(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+  }, []);
+
   return (
-    <div>
-      <div className="interviewNote">
-        <div className="noteTitle" >Interview Note</div>
-        <div className="companyName">Company Name</div>
-        
-        <textarea className="noteInput" placeholder="Write a note"></textarea>
+    <React.Fragment>
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        {companyName.map((value, index) => (
+          <ListItem
+            key={index}
+            disableGutters
+            secondaryAction={
+              <IconButton edge="end" aria-label="comment">
+                <CommentIcon />
+              </IconButton>
+            }
+          >
+            <ListItemText primary={`Line item ${value.name}`} />
+          </ListItem>
+        ))}
+      </List>
+      <div>
+        <div className="interviewNote">
+          <div className="noteTitle">Interview Note</div>
+          <div className="companyName">Company Name</div>
+
+          <textarea className="noteInput" placeholder="Write a note"></textarea>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
