19c3340c88f27ea01ef0342b7a73a70fbac75141
Jay Kim <jbkim3510@gmail.com>
2025-05-06 16:44:41 -0700

    add appliction count number and button

--- src/ApplicationCount/Count.js
+++ src/ApplicationCount/Count.js
@@ -2,7 +2,8 @@ import "../App.css";
 import React, { useState } from "react";
 import "./Count.css";
 import { Checkbox } from "@mui/material";
-import { NavItem } from "react-bootstrap";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
@@ -12,9 +13,15 @@ function Count({ checked, setChecked }) {
     updated[index] = e.target.checked;
     setChecked(updated);
   };
+
+  function appCountSaveButton () {
+    
+  }
   return (
     <React.Fragment>
-      <div className= "applicationCount"Style="margin-top:30px;">Apply Count</div>
+      <div className="applicationCount" Style="margin-top:30px;">
+        Apply Count {checked.filter(Boolean).length}
+      </div>
       <div
         Style="margin-top:-10px"
         id="checkBox-container"
@@ -23,17 +30,28 @@ function Count({ checked, setChecked }) {
         <div className="float-child">
           <div Style="margin-top:10px; margin-right:4px">1. </div>
         </div>
-        <div class="float-child">
-          <div>
-            {checked.map((item, index) => (
-              <Checkbox
-                key={index}
-                checked={item}
-                onChange={handleCheckBox(index)}
-                {...label}
-              />
-            ))}
+        <div className="checkBoxContainer">
+          <div class="float-child">
+            <div>
+              {checked.map((item, index) => (
+                <Checkbox
+                  key={index}
+                  checked={item}
+                  onChange={handleCheckBox(index)}
+                  {...label}
+                />
+              ))}
+            </div>
           </div>
+          <Stack className="seeNoteButton" spacing={2} direction="row">
+            <Button
+              variant="contained"
+              onClick={() => appCountSaveButton()}
+              Style="margin-top: 10px"
+            >
+              See Note
+            </Button>
+          </Stack>
         </div>
       </div>
     </React.Fragment>
